96a49fe4ad65e506a13603322a04a8253ff916eb
Junior SEVERE <severejunior2017@gmail.com>
2025-07-25 20:48:23 -0400

    final version

--- mail/admin.py
+++ mail/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import User, Email
+
+# Enregistre le modèle User
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'is_staff', 'date_joined')
+    list_filter = ('is_staff', 'is_superuser', 'date_joined')
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email',)
+
+# Enregistre le modèle Email
+@admin.register(Email)
+class EmailAdmin(admin.ModelAdmin):
+    list_display = ('subject', 'sender', 'get_recipients', 'timestamp', 'read', 'archived')
+    list_filter = ('read', 'archived', 'timestamp')
+    search_fields = ('subject', 'sender__email', 'body')
+    ordering = ('-timestamp',)
+    readonly_fields = ('timestamp',)
+    
+    def get_recipients(self, obj):
+        return ", ".join([user.email for user in obj.recipients.all()])
+    get_recipients.short_description = 'Recipients'
